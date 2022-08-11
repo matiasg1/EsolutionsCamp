ab246a85b38631d487d2f66237740d2b90f3a7d6
matiasg1 <mati.gerez7@gmail.com>
2022-08-11 11:48:04 -0300

    Primera parte

---
+++ clase04/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../course';
+import { CourseService } from '../course.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  courses: Course[] = [];
+
+  constructor(private courseService: CourseService) { }
+
+  getCourses(): void {
+   this.courseService.getCoursesInProgress().subscribe(
+      courses => this.courses = courses
+   );
+  }
+
+  ngOnInit(): void {
+    this.getCourses();
+  }
+
+}
